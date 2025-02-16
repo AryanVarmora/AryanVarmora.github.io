97a52a79c00f0ac64982af870267ed7e1a268844
AryanVarmora <aryanvarmora66@gmail.com>
2025-02-16 16:18:50 -0500

    removed

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  base: '/AryanVarmora.github.io/', // Set repository name as base path
-  build: {
-    outDir: 'dist',
-  },
-});
